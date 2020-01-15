918afc719502600788f66b1fa69ae19ace454ab4
Roman Frołow <rofrol@gmail.com>
2020-01-15 14:10:15 +0100

    Update tutorial4 to winit 0.20

--- code/beginner/tutorial4-buffer/src/challenge.rs
+++ code/beginner/tutorial4-buffer/src/challenge.rs
@@ -1,6 +1,6 @@
 use winit::{
     event::*,
-    event_loop::{EventLoop, ControlFlow},
+    event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
 
@@ -28,24 +28,35 @@ impl Vertex {
                     shader_location: 1,
                     format: wgpu::VertexFormat::Float3,
                 },
-            ]
+            ],
         }
     }
 }
 
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [-0.0868241, -0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
-    Vertex { position: [-0.49513406, -0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
-    Vertex { position: [-0.21918549, 0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
-    Vertex { position: [0.35966998, 0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
-    Vertex { position: [0.44147372, -0.2347359, 0.0],color: [0.5, 0.0, 0.5] }, // E
+    Vertex {
+        position: [-0.0868241, -0.49240386, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // A
+    Vertex {
+        position: [-0.49513406, -0.06958647, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // B
+    Vertex {
+        position: [-0.21918549, 0.44939706, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // C
+    Vertex {
+        position: [0.35966998, 0.3473291, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // D
+    Vertex {
+        position: [0.44147372, -0.2347359, 0.0],
+        color: [0.5, 0.0, 0.5],
+    }, // E
 ];
 
-const INDICES: &[u16] = &[
-    0, 1, 4,
-    1, 2, 4,
-    2, 3, 4,
-];
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
 
 struct State {
     surface: wgpu::Surface,
@@ -64,22 +75,18 @@ struct State {
     challenge_index_buffer: wgpu::Buffer,
     num_challenge_indices: u32,
     use_complex: bool,
-
-    hidpi_factor: f64,
-    size: winit::dpi::LogicalSize,
 }
 
 impl State {
     fn new(window: &Window) -> Self {
-        let hidpi_factor = window.hidpi_factor();
         let size = window.inner_size();
-        let physical_size = size.to_physical(hidpi_factor);
 
         let surface = wgpu::Surface::create(window);
 
         let adapter = wgpu::Adapter::request(&wgpu::RequestAdapterOptions {
             ..Default::default()
-        }).unwrap();
+        })
+        .unwrap();
 
         let (device, queue) = adapter.request_device(&wgpu::DeviceDescriptor {
             extensions: wgpu::Extensions {
@@ -91,8 +98,8 @@ impl State {
         let sc_desc = wgpu::SwapChainDescriptor {
             usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
             format: wgpu::TextureFormat::Bgra8UnormSrgb,
-            width: physical_size.width.round() as u32,
-            height: physical_size.height.round() as u32,
+            width: size.width,
+            height: size.height,
             present_mode: wgpu::PresentMode::Vsync,
         };
         let swap_chain = device.create_swap_chain(&surface, &sc_desc);
@@ -106,9 +113,10 @@ impl State {
         let vs_module = device.create_shader_module(&vs_data);
         let fs_module = device.create_shader_module(&fs_data);
 
-        let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            bind_group_layouts: &[],
-        });
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                bind_group_layouts: &[],
+            });
 
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             layout: &render_pipeline_layout,
@@ -128,19 +136,15 @@ impl State {
                 depth_bias_clamp: 0.0,
             }),
             primitive_topology: wgpu::PrimitiveTopology::TriangleList,
-            color_states: &[
-                wgpu::ColorStateDescriptor {
-                    format: sc_desc.format,
-                    color_blend: wgpu::BlendDescriptor::REPLACE,
-                    alpha_blend: wgpu::BlendDescriptor::REPLACE,
-                    write_mask: wgpu::ColorWrite::ALL,
-                },
-            ],
+            color_states: &[wgpu::ColorStateDescriptor {
+                format: sc_desc.format,
+                color_blend: wgpu::BlendDescriptor::REPLACE,
+                alpha_blend: wgpu::BlendDescriptor::REPLACE,
+                write_mask: wgpu::ColorWrite::ALL,
+            }],
             depth_stencil_state: None,
             index_format: wgpu::IndexFormat::Uint16,
-            vertex_buffers: &[
-                Vertex::desc(),
-            ],
+            vertex_buffers: &[Vertex::desc()],
             sample_count: 1,
             sample_mask: !0,
             alpha_to_coverage_enabled: false,
@@ -156,26 +160,21 @@ impl State {
 
         let num_vertices = 16;
         let angle = std::f32::consts::PI * 2.0 / num_vertices as f32;
-        let challenge_verts = (0..num_vertices).map(|i| {
-            let theta = angle * i as f32;
-            Vertex {
-                position: [
-                    0.5 * theta.cos(),
-                    0.5 * theta.sin(),
-                    0.0,
-                ],
-                color: [
-                    (1.0 + theta.cos()) / 2.0,
-                    (1.0 + theta.sin()) / 2.0,
-                    1.0,
-                ]
-            }
-        }).collect::<Vec<_>>();
-        
+        let challenge_verts = (0..num_vertices)
+            .map(|i| {
+                let theta = angle * i as f32;
+                Vertex {
+                    position: [0.5 * theta.cos(), 0.5 * theta.sin(), 0.0],
+                    color: [(1.0 + theta.cos()) / 2.0, (1.0 + theta.sin()) / 2.0, 1.0],
+                }
+            })
+            .collect::<Vec<_>>();
+
         let num_triangles = num_vertices - 2;
-        let challenge_indices = (1u16..num_triangles+1).into_iter().flat_map(|i| {
-            vec![i + 1, i, 0]
-        }).collect::<Vec<_>>();
+        let challenge_indices = (1u16..num_triangles + 1)
+            .into_iter()
+            .flat_map(|i| vec![i + 1, i, 0])
+            .collect::<Vec<_>>();
         let num_challenge_indices = challenge_indices.len() as u32;
 
         let challenge_vertex_buffer = device
@@ -201,32 +200,24 @@ impl State {
             challenge_index_buffer,
             num_challenge_indices,
             use_complex,
-            hidpi_factor,
-            size,
         }
     }
 
-    fn update_hidpi_and_resize(&mut self, new_hidpi_factor: f64) {
-        self.hidpi_factor = new_hidpi_factor;
-        self.resize(self.size);
-    }
-
-    fn resize(&mut self, new_size: winit::dpi::LogicalSize) {
-        let physical_size = new_size.to_physical(self.hidpi_factor);
-        self.size = new_size;
-        self.sc_desc.width = physical_size.width.round() as u32;
-        self.sc_desc.height = physical_size.height.round() as u32;
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        self.sc_desc.width = new_size.width;
+        self.sc_desc.height = new_size.height;
         self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
         match event {
             WindowEvent::KeyboardInput {
-                input: KeyboardInput {
-                    state,
-                    virtual_keycode: Some(VirtualKeyCode::Space),
-                    ..
-                },
+                input:
+                    KeyboardInput {
+                        state,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
                 ..
             } => {
                 self.use_complex = *state == ElementState::Pressed;
@@ -236,33 +227,29 @@ impl State {
         }
     }
 
-    fn update(&mut self) {
-
-    }
+    fn update(&mut self) {}
 
     fn render(&mut self) {
         let frame = self.swap_chain.get_next_texture();
 
-        let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-            todo: 0,
-        });
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { todo: 0 });
 
         {
             let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                color_attachments: &[
-                    wgpu::RenderPassColorAttachmentDescriptor {
-                        attachment: &frame.view,
-                        resolve_target: None,
-                        load_op: wgpu::LoadOp::Clear,
-                        store_op: wgpu::StoreOp::Store,
-                        clear_color: wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        },
-                    }
-                ],
+                color_attachments: &[wgpu::RenderPassColorAttachmentDescriptor {
+                    attachment: &frame.view,
+                    resolve_target: None,
+                    load_op: wgpu::LoadOp::Clear,
+                    store_op: wgpu::StoreOp::Store,
+                    clear_color: wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    },
+                }],
                 depth_stencil_attachment: None,
             });
 
@@ -270,16 +257,12 @@ impl State {
 
             let data = if self.use_complex {
                 (
-                    &self.challenge_vertex_buffer, 
-                    &self.challenge_index_buffer, 
-                    self.num_challenge_indices
+                    &self.challenge_vertex_buffer,
+                    &self.challenge_index_buffer,
+                    self.num_challenge_indices,
                 )
             } else {
-                (
-                    &self.vertex_buffer,
-                    &self.index_buffer,
-                    self.num_indices,
-                )
+                (&self.vertex_buffer, &self.index_buffer, self.num_indices)
             };
             render_pass.set_vertex_buffers(0, &[(data.0, 0)]);
             render_pass.set_index_buffer(data.1, 0);
@@ -287,60 +270,51 @@ impl State {
             render_pass.draw_indexed(0..data.2, 0, 0..1);
         }
 
-        self.queue.submit(&[
-            encoder.finish()
-        ]);
+        self.queue.submit(&[encoder.finish()]);
     }
 }
 
 fn main() {
     let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .build(&event_loop)
-        .unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
     let mut state = State::new(&window);
-    
-    event_loop.run(move |event, _, control_flow| {
-        match event {
-            Event::WindowEvent {
-                ref event,
-                window_id,
-            } if window_id == window.id() => if state.input(event) {
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
+            if state.input(event) {
                 *control_flow = ControlFlow::Wait;
-            } else { 
+            } else {
                 match event {
                     WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
-                    WindowEvent::KeyboardInput {
-                        input,
-                        ..
-                    } => {
-                        match input {
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            } => *control_flow = ControlFlow::Exit,
-                            _ => *control_flow = ControlFlow::Wait,
-                        }
-                    }
+                    WindowEvent::KeyboardInput { input, .. } => match input {
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+                        _ => *control_flow = ControlFlow::Wait,
+                    },
                     WindowEvent::Resized(logical_size) => {
                         state.resize(*logical_size);
                         *control_flow = ControlFlow::Wait;
                     }
-                    WindowEvent::HiDpiFactorChanged(new_hidpi_factor) => {
-                        state.update_hidpi_and_resize(*new_hidpi_factor);
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        state.resize(**new_inner_size);
                         *control_flow = ControlFlow::Wait;
                     }
                     _ => *control_flow = ControlFlow::Wait,
                 }
             }
-            Event::EventsCleared => {
-                state.update();
-                state.render();
-                *control_flow = ControlFlow::Wait;
-            }
-            _ => *control_flow = ControlFlow::Wait,
         }
+        Event::RedrawRequested(_) => {
+            state.update();
+            state.render();
+            *control_flow = ControlFlow::Wait;
+        }
+        _ => *control_flow = ControlFlow::Wait,
     });
 }
