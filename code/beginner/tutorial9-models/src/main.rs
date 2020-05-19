67f163aaec0f141ecff9be415413401581c3f2a8
Ben Hansen <bhbenjaminhansen@gmail.com>
2020-05-19 11:46:04 -0600

    fixed crash in later example code

--- code/beginner/tutorial9-models/src/main.rs
+++ code/beginner/tutorial9-models/src/main.rs
@@ -423,12 +423,12 @@ impl State {
     }
 
     fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        self.depth_texture = texture::Texture::create_depth_texture(&self.device, &self.sc_desc, "depth_texture");
         self.camera.aspect = self.sc_desc.width as f32 / self.sc_desc.height as f32;
         self.size = new_size;
         self.sc_desc.width = new_size.width;
         self.sc_desc.height = new_size.height;
         self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+        self.depth_texture = texture::Texture::create_depth_texture(&self.device, &self.sc_desc, "depth_texture");
     }
     fn input(&mut self, event: &WindowEvent) -> bool {
         self.camera_controller.process_events(event)
