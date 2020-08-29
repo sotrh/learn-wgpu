0fbb71efb608121d4427f61f362e8f11ae355409
Ben Hansen <bhbenjaminhansen@gmail.com>
2020-08-28 19:47:16 -0600

    migrated tutorial4

--- code/beginner/tutorial4-buffer/src/main.rs
+++ code/beginner/tutorial4-buffer/src/main.rs
@@ -229,8 +229,8 @@ impl State {
             });
 
             render_pass.set_pipeline(&self.render_pipeline);
-            render_pass.set_index_buffer(self.index_buffer.slice(..));
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..));
             render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
