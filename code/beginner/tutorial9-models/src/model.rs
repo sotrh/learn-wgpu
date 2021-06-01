8a01d0e519e5e25cf083690cab87e61297336f92
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-06-01 13:53:03 +0100

    Update model loading tutorial to tobj 3.0.x

--- code/beginner/tutorial9-models/src/model.rs
+++ code/beginner/tutorial9-models/src/model.rs
@@ -1,6 +1,7 @@
 use anyhow::*;
 use std::ops::Range;
 use std::path::Path;
+use tobj::LoadOptions;
 use wgpu::util::DeviceExt;
 
 use crate::texture;
@@ -70,7 +71,14 @@ impl Model {
         layout: &wgpu::BindGroupLayout,
         path: P,
     ) -> Result<Self> {
-        let (obj_models, obj_materials) = tobj::load_obj(path.as_ref(), true)?;
+        let (obj_models, obj_materials) = tobj::load_obj(path.as_ref(), &LoadOptions {
+                triangulate: true,
+                single_index: true,
+                ..Default::default()
+            },
+        )?;
+
+        let obj_materials = obj_materials?;
 
         // We're assuming that the texture files are stored with the obj file
         let containing_folder = path.as_ref().parent().context("Directory has no parent")?;
