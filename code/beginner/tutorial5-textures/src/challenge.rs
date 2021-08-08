7f5dbe7245f421f320d52ae422e0548453742fd2
Rokonio <61879190+rokonio@users.noreply.github.com>
2021-08-09 00:32:21 +0200

    Fixing #200

--- code/beginner/tutorial5-textures/src/challenge.rs
+++ code/beginner/tutorial5-textures/src/challenge.rs
@@ -268,11 +268,13 @@ impl State {
         }
     }
 
-    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
-        self.size = new_size;
-        self.sc_desc.width = new_size.width;
-        self.sc_desc.height = new_size.height;
-        self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
+        if new_size.width > 0 && new_size.height > 0 {
+            self.size = new_size;
+            self.sc_desc.width = new_size.width;
+            self.sc_desc.height = new_size.height;
+            self.swap_chain = self.device.create_swap_chain(&self.surface, &self.sc_desc);
+        }
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
