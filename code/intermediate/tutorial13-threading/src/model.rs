212da09b155fae6b7c5cba4297472f3c0950f487
roudy16 <roudy@umich.edu>
2022-03-01 21:50:15 +0000

    enable right-handed normal maps

--- code/intermediate/tutorial13-threading/src/model.rs
+++ code/intermediate/tutorial13-threading/src/model.rs
@@ -220,6 +220,9 @@ impl Model {
                 let r = 1.0 / (delta_uv1.x * delta_uv2.y - delta_uv1.y * delta_uv2.x);
                 let tangent = (delta_pos1 * delta_uv2.y - delta_pos2 * delta_uv1.y) * r;
                 let bitangent = (delta_pos2 * delta_uv1.x - delta_pos1 * delta_uv2.x) * r;
+                // We flip the bitangent to enable right-handed normal
+                // maps with wgpu texture coordinate system
+                let flipped_bitangent = bitangent * -1.0;
 
                 // We'll use the same tangent/bitangent for each vertex in the triangle
                 vertices[c[0] as usize].tangent =
@@ -229,11 +232,11 @@ impl Model {
                 vertices[c[2] as usize].tangent =
                     (tangent + cgmath::Vector3::from(vertices[c[2] as usize].tangent)).into();
                 vertices[c[0] as usize].bitangent =
-                    (bitangent + cgmath::Vector3::from(vertices[c[0] as usize].bitangent)).into();
+                    (flipped_bitangent + cgmath::Vector3::from(vertices[c[0] as usize].bitangent)).into();
                 vertices[c[1] as usize].bitangent =
-                    (bitangent + cgmath::Vector3::from(vertices[c[1] as usize].bitangent)).into();
+                    (flipped_bitangent + cgmath::Vector3::from(vertices[c[1] as usize].bitangent)).into();
                 vertices[c[2] as usize].bitangent =
-                    (bitangent + cgmath::Vector3::from(vertices[c[2] as usize].bitangent)).into();
+                    (flipped_bitangent + cgmath::Vector3::from(vertices[c[2] as usize].bitangent)).into();
 
                 // Used to average the tangents/bitangents
                 triangles_included[c[0] as usize] += 1;
