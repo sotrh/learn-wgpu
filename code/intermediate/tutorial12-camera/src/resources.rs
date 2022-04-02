0d5ac83d1f8bea1bc9c0a8226569727e461231fd
tata <8picoz@tata.pw>
2022-04-03 03:13:40 +0900

    Except cfg_if crate dependencies in tutorial12

--- code/intermediate/tutorial12-camera/src/resources.rs
+++ code/intermediate/tutorial12-camera/src/resources.rs
@@ -1,6 +1,5 @@
 use std::io::{BufReader, Cursor};
 
-use cfg_if::cfg_if;
 use wgpu::util::DeviceExt;
 
 use crate::{model, texture};
@@ -17,41 +16,45 @@ use crate::{model, texture};
     base.join(file_name).unwrap()
 }
 
+#[cfg(target_arch = "wasm32")]
 pub async fn load_string(file_name: &str) -> anyhow::Result<String> {
-    cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            let url = format_url(file_name);
-            let txt = reqwest::get(url)
-                .await?
-                .text()
-                .await?;
-        } else {
-            let path = std::path::Path::new(env!("OUT_DIR"))
-                .join("res")
-                .join(file_name);
-            let txt = std::fs::read_to_string(path)?;
-        }
-    }
+    let url = format_url(file_name);
+    let txt = reqwest::get(url)
+        .await?
+        .text()
+        .await?;
 
     Ok(txt)
 }
 
+#[cfg(not(target_arch = "wasm32"))]
+pub async fn load_string(file_name: &str) -> anyhow::Result<String> {
+    let path = std::path::Path::new(env!("OUT_DIR"))
+            .join("res")
+            .join(file_name);
+    let txt = std::fs::read_to_string(path)?;
+
+    Ok(txt)
+}
+
+#[cfg(target_arch = "wasm32")]
 pub async fn load_binary(file_name: &str) -> anyhow::Result<Vec<u8>> {
-    cfg_if! {
-        if #[cfg(target_arch = "wasm32")] {
-            let url = format_url(file_name);
-            let data = reqwest::get(url)
-                .await?
-                .bytes()
-                .await?
-                .to_vec();
-        } else {
-            let path = std::path::Path::new(env!("OUT_DIR"))
-                .join("res")
-                .join(file_name);
-            let data = std::fs::read(path)?;
-        }
-    }
+    let url = format_url(file_name);
+    let data = reqwest::get(url)
+        .await?
+        .bytes()
+        .await?
+        .to_vec();
+
+    Ok(data)
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub async fn load_binary(file_name: &str) -> anyhow::Result<Vec<u8>> {
+    let path = std::path::Path::new(env!("OUT_DIR"))
+        .join("res")
+        .join(file_name);
+    let data = std::fs::read(path)?;
 
     Ok(data)
 }
