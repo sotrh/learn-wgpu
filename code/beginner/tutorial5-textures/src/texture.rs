62b9fe0aeb3bf2efe0a9c50b2842368f8fa7b4cc
Bot-Kerem <72509655+Bot-Kerem@users.noreply.github.com>
2023-06-18 20:23:31 +0300

    Update texture.rs

--- code/beginner/tutorial5-textures/src/texture.rs
+++ code/beginner/tutorial5-textures/src/texture.rs
@@ -56,8 +56,8 @@ impl Texture {
             &rgba,
             wgpu::ImageDataLayout {
                 offset: 0,
-                bytes_per_row: Some(4 * dimensions.0),
-                rows_per_image: Some(dimensions.1),
+                bytes_per_row: std::num::NonZeroU32::new(4 * dimensions.0).map(|n| n.get()),
+                rows_per_image: std::num::NonZeroU32::new(dimensions.1).map(|n| n.get()),
             },
             size,
         );
