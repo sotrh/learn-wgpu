893f6174924ffd5713dc52f740fd7039b13b509d
Melody <edward.lenzner@gmail.com>
2024-10-23 20:14:58 +1100

    Improve CameraController code

--- code/beginner/tutorial6-uniforms/src/lib.rs
+++ code/beginner/tutorial6-uniforms/src/lib.rs
@@ -75,14 +75,18 @@ pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
     0.0, 0.0, 0.0, 1.0,
 );
 
-struct Camera {
-    eye: cgmath::Point3<f32>,
-    target: cgmath::Point3<f32>,
-    up: cgmath::Vector3<f32>,
-    aspect: f32,
-    fovy: f32,
-    znear: f32,
-    zfar: f32,
+pub struct Camera {
+    pub eye: cgmath::Point3<f32>,
+    pub target: cgmath::Point3<f32>,
+    pub up: cgmath::Vector3<f32>,
+    pub aspect: f32,
+    pub fovy: f32,
+    pub znear: f32,
+    pub zfar: f32,
+
+    // If you're wondering, we're not using a Quaternion because that adds an extra level of complication
+    // when we don't need to worry about gimbal lock - all rotations will be manual, so it won't affect any calculations
+    pub rotation: cgmath::Vector3<f32>
 }
 
 impl Camera {
@@ -112,30 +116,35 @@ impl CameraUniform {
     }
 }
 
-struct CameraController {
-    speed: f32,
-    is_up_pressed: bool,
-    is_down_pressed: bool,
-    is_forward_pressed: bool,
-    is_backward_pressed: bool,
-    is_left_pressed: bool,
-    is_right_pressed: bool,
+pub struct CameraController {
+    pub speed: f32,
+    pub is_forward_pressed: bool,
+    pub is_backward_pressed: bool,
+    pub is_left_pressed: bool,
+    pub is_right_pressed: bool,
+    pub is_up_pressed: bool,
+    pub is_down_pressed: bool,
+    // tilt the camera on the z axis clockwise and counter-clockwise
+    pub is_zcw_pressed: bool,
+    pub is_zccw_pressed: bool,
 }
 
 impl CameraController {
-    fn new(speed: f32) -> Self {
+    pub fn new(speed: f32) -> Self {
         Self {
             speed,
-            is_up_pressed: false,
-            is_down_pressed: false,
             is_forward_pressed: false,
             is_backward_pressed: false,
             is_left_pressed: false,
             is_right_pressed: false,
+            is_up_pressed: false,
+            is_down_pressed: false,
+            is_zcw_pressed: false,
+            is_zccw_pressed: false,
         }
     }
 
-    fn process_events(&mut self, event: &WindowEvent) -> bool {
+    pub fn process_events(&mut self, event: &WindowEvent) -> bool {
         match event {
             WindowEvent::KeyboardInput {
                 event:
@@ -147,16 +156,7 @@ impl CameraController {
                 ..
             } => {
                 let is_pressed = *state == ElementState::Pressed;
-                match keycode {
-                    KeyCode::Space => {
-                        self.is_up_pressed = is_pressed;
-                        true
-                    }
-                    KeyCode::ShiftLeft => {
-                        self.is_down_pressed = is_pressed;
-                        true
-                    }
-                    KeyCode::KeyW | KeyCode::ArrowUp => {
+                match keycode {KeyCode::KeyW | KeyCode::ArrowUp => {
                         self.is_forward_pressed = is_pressed;
                         true
                     }
@@ -172,6 +172,22 @@ impl CameraController {
                         self.is_right_pressed = is_pressed;
                         true
                     }
+                    KeyCode::KeyE => {
+                        self.is_up_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyQ => {
+                        self.is_down_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyC => {
+                        self.is_zcw_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyZ => {
+                        self.is_zccw_pressed = is_pressed;
+                        true
+                    }
                     _ => false,
                 }
             }
@@ -179,13 +195,13 @@ impl CameraController {
         }
     }
 
-    fn update_camera(&self, camera: &mut Camera) {
-        use cgmath::InnerSpace;
+    pub fn update_camera(&self, camera: &mut Camera) {
+        use cgmath::{InnerSpace, Vector3};
         let forward = camera.target - camera.eye;
         let forward_norm = forward.normalize();
         let forward_mag = forward.magnitude();
 
-        // Prevents glitching when camera gets too close to the
+        // Prevents glitching when the camera gets too close to the
         // center of the scene.
         if self.is_forward_pressed && forward_mag > self.speed {
             camera.eye += forward_norm * self.speed;
@@ -194,21 +210,80 @@ impl CameraController {
             camera.eye -= forward_norm * self.speed;
         }
 
-        let right = forward_norm.cross(camera.up);
-
-        // Redo radius calc in case the up/ down is pressed.
-        let forward = camera.target - camera.eye;
-        let forward_mag = forward.magnitude();
-
         if self.is_right_pressed {
-            // Rescale the distance between the target and eye so
-            // that it doesn't change. The eye therefore still
-            // lies on the circle made by the target and eye.
-            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
+            camera.rotation.y += self.speed;
         }
         if self.is_left_pressed {
-            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+            camera.rotation.y -= self.speed;
         }
+        if self.is_down_pressed {
+            camera.rotation.x += self.speed;
+        }     
+        if self.is_up_pressed {
+            camera.rotation.x -= self.speed;
+        }
+        if self.is_zcw_pressed {
+            camera.rotation.z += self.speed;
+        }     
+        if self.is_zccw_pressed {
+            camera.rotation.z -= self.speed;
+        }
+
+        // Recalculate the forward vector based on its new direction and magnitude
+        let forward = Vector3::new(
+            forward_mag * camera.rotation.x.cos() * camera.rotation.y.cos(),
+            forward_mag * camera.rotation.x.sin(),
+            forward_mag * camera.rotation.x.cos() * camera.rotation.y.sin(),
+        );        
+        // Reposition eye so that forward points at the target again
+        camera.eye = camera.target - forward;
+        camera.up = self.recalculate_up(forward, camera);
+    }
+
+    // Recalculates up vector based on new rotations
+    fn recalculate_up(&self, forward: cgmath::Vector3<f32>, camera: &mut Camera) -> cgmath::Vector3<f32> {
+        use cgmath::{InnerSpace, Vector3};
+        use std::f32::consts::PI;
+        // Precompute values which are used a lot (and expensive)
+        let camera_rotation_x = camera.rotation.x.rem_euclid(2.0 * PI);
+        let sin_z = camera.rotation.z.sin();
+        let cos_z = camera.rotation.z.cos();
+
+        // Calculate the right vector
+        // We use a global up vector because the real up vector actually doesn't effect the right vector (think about it)
+        let mut right = forward.cross(Vector3::new(0.0, 1.0, 0.0)).normalize();
+        // Change the signs of x and z so they work with every rotation
+        // (each octant has different signs that follow this rule based on the forward vector)
+        right.x = right.x.abs() * forward.z.signum();
+        right.z = right.z.abs() * -forward.x.signum();
+
+        // Calculate the up vector similarly to the right vector, only with different signs
+        let mut up = forward.cross(right).normalize();
+        up.x = up.x.abs() * forward.x.signum();
+        up.y = up.y.abs();
+        up.z = up.z.abs() * forward.z.signum();
+
+        // Flip the up vector values if the camera is rotated upside down by the x axis
+        // These fractions of PI come from trial and error and seeing which rotations break the up vector
+        // If anyone knows their significance, please tell me (maybe I messed up the octant signs?)
+        if (camera_rotation_x > 0.25 * PI && camera_rotation_x <= 0.5 * PI)
+        || (camera_rotation_x >= 0.75 * PI && camera_rotation_x < 1.5 * PI) { up *= -1.0; }
+
+        // Rotate the up vector around the forward vector
+        // Effectively applies z rotation after the fact, 
+        // so we dont have to deal with that messing up the previous calculations
+        let forward_dot = forward.dot(forward);
+        let parallel = (up.dot(forward) / forward_dot) * forward;
+        let orthogonal = up - parallel;
+        let w = forward.cross(orthogonal);
+        let orthogonal_magnitude = orthogonal.magnitude();
+
+        let x1 = cos_z / orthogonal_magnitude;
+        let x2 = sin_z / w.magnitude();
+        let orthogonal_rotated = orthogonal_magnitude * (x1 * orthogonal + x2 * w);
+        up = orthogonal_rotated + parallel;
+
+        up
     }
 }
 
@@ -348,8 +423,9 @@ impl<'a> State<'a> {
             fovy: 45.0,
             znear: 0.1,
             zfar: 100.0,
+            rotation: cgmath::Vector3::new(0.0, 0.0, 0.0)
         };
-        let camera_controller = CameraController::new(0.2);
+        let camera_controller = CameraController::new(0.01);
 
         let mut camera_uniform = CameraUniform::new();
         camera_uniform.update_view_proj(&camera);
--- docs/beginner/tutorial6-uniforms/README.md
+++ docs/beginner/tutorial6-uniforms/README.md
@@ -277,29 +277,58 @@ If you run the code right now, you should get something like this.
 
 ![./static-tree.png](./static-tree.png)
 
-The shape's less stretched now, but it's still pretty static. You can experiment with moving the camera position around, but most cameras in games move around. Since this tutorial is about using wgpu and not how to process user input, I'm just going to post the `CameraController` code below.
+The shape's less stretched now, but it's still pretty static. You can experiment with moving the camera position around, but most cameras in games move around. 
+
+First, we'll update our camera to have a rotation property for our controller:
+```rust
+pub struct Camera {
+    pub eye: cgmath::Point3<f32>,
+    pub target: cgmath::Point3<f32>,
+    pub up: cgmath::Vector3<f32>,
+    pub aspect: f32,
+    pub fovy: f32,
+    pub znear: f32,
+    pub zfar: f32,
+
+    // NEW!
+    // If you're wondering, we're not using a Quaternion because that adds an extra level of complication
+    // when we don't need to worry about gimbal lock - all rotations will be manual, so it won't affect any calculations
+    pub rotation: cgmath::Vector3<f32>
+}
+```
+
+Then we'll add our controller struct. Since this tutorial is about using wgpu and not how to process user input, I'm just going to post the `CameraController` code below.
 
 ```rust
-struct CameraController {
-    speed: f32,
-    is_forward_pressed: bool,
-    is_backward_pressed: bool,
-    is_left_pressed: bool,
-    is_right_pressed: bool,
+pub struct CameraController {
+    pub speed: f32,
+    pub is_forward_pressed: bool,
+    pub is_backward_pressed: bool,
+    pub is_left_pressed: bool,
+    pub is_right_pressed: bool,
+    pub is_up_pressed: bool,
+    pub is_down_pressed: bool,
+    // tilt the camera on the z axis clockwise and counter-clockwise
+    pub is_zcw_pressed: bool,
+    pub is_zccw_pressed: bool,
 }
 
 impl CameraController {
-    fn new(speed: f32) -> Self {
+    pub fn new(speed: f32) -> Self {
         Self {
             speed,
             is_forward_pressed: false,
             is_backward_pressed: false,
             is_left_pressed: false,
             is_right_pressed: false,
+            is_up_pressed: false,
+            is_down_pressed: false,
+            is_zcw_pressed: false,
+            is_zccw_pressed: false,
         }
     }
 
-    fn process_events(&mut self, event: &WindowEvent) -> bool {
+    pub fn process_events(&mut self, event: &WindowEvent) -> bool {
         match event {
             WindowEvent::KeyboardInput {
                 event:
@@ -327,6 +356,22 @@ impl CameraController {
                         self.is_right_pressed = is_pressed;
                         true
                     }
+                    KeyCode::KeyE => {
+                        self.is_up_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyQ => {
+                        self.is_down_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyC => {
+                        self.is_zcw_pressed = is_pressed;
+                        true
+                    }
+                    KeyCode::KeyZ => {
+                        self.is_zccw_pressed = is_pressed;
+                        true
+                    }
                     _ => false,
                 }
             }
@@ -334,8 +379,8 @@ impl CameraController {
         }
     }
 
-    fn update_camera(&self, camera: &mut Camera) {
-        use cgmath::InnerSpace;
+    pub fn update_camera(&self, camera: &mut Camera) {
+        use cgmath::{InnerSpace, Vector3};
         let forward = camera.target - camera.eye;
         let forward_norm = forward.normalize();
         let forward_mag = forward.magnitude();
@@ -349,26 +394,85 @@ impl CameraController {
             camera.eye -= forward_norm * self.speed;
         }
 
-        let right = forward_norm.cross(camera.up);
-
-        // Redo radius calc in case the forward/backward is pressed.
-        let forward = camera.target - camera.eye;
-        let forward_mag = forward.magnitude();
-
         if self.is_right_pressed {
-            // Rescale the distance between the target and the eye so 
-            // that it doesn't change. The eye, therefore, still 
-            // lies on the circle made by the target and eye.
-            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
+            camera.rotation.y += self.speed;
         }
         if self.is_left_pressed {
-            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+            camera.rotation.y -= self.speed;
         }
+        if self.is_down_pressed {
+            camera.rotation.x += self.speed;
+        }     
+        if self.is_up_pressed {
+            camera.rotation.x -= self.speed;
+        }
+        if self.is_zcw_pressed {
+            camera.rotation.z += self.speed;
+        }     
+        if self.is_zccw_pressed {
+            camera.rotation.z -= self.speed;
+        }
+
+        // Recalculate the forward vector based on its new direction and magnitude
+        let forward = Vector3::new(
+            forward_mag * camera.rotation.x.cos() * camera.rotation.y.cos(),
+            forward_mag * camera.rotation.x.sin(),
+            forward_mag * camera.rotation.x.cos() * camera.rotation.y.sin(),
+        );        
+        // Reposition eye so that forward points at the target again
+        camera.eye = camera.target - forward;
+        camera.up = self.recalculate_up(forward, camera);
+    }
+
+    // Recalculates up vector based on new rotations
+    fn recalculate_up(&self, forward: cgmath::Vector3<f32>, camera: &mut Camera) -> cgmath::Vector3<f32> {
+        use cgmath::{InnerSpace, Vector3};
+        use std::f32::consts::PI;
+        // Precompute values which are used a lot (and expensive)
+        let camera_rotation_x = camera.rotation.x.rem_euclid(2.0 * PI);
+        let sin_z = camera.rotation.z.sin();
+        let cos_z = camera.rotation.z.cos();
+
+        // Calculate the right vector
+        // We use a global up vector because the real up vector actually doesn't effect the right vector (think about it)
+        let mut right = forward.cross(Vector3::new(0.0, 1.0, 0.0)).normalize();
+        // Change the signs of x and z so they work with every rotation
+        // (each octant has different signs that follow this rule based on the forward vector)
+        right.x = right.x.abs() * forward.z.signum();
+        right.z = right.z.abs() * -forward.x.signum();
+
+        // Calculate the up vector similarly to the right vector, only with different signs
+        let mut up = forward.cross(right).normalize();
+        up.x = up.x.abs() * forward.x.signum();
+        up.y = up.y.abs();
+        up.z = up.z.abs() * forward.z.signum();
+
+        // Flip the up vector values if the camera is rotated upside down by the x axis
+        // These fractions of PI come from trial and error and seeing which rotations break the up vector
+        // If anyone knows their significance, please tell me (maybe I messed up the octant signs?)
+        if (camera_rotation_x > 0.25 * PI && camera_rotation_x <= 0.5 * PI)
+        || (camera_rotation_x >= 0.75 * PI && camera_rotation_x < 1.5 * PI) { up *= -1.0; }
+
+        // Rotate the up vector around the forward vector
+        // Effectively applies z rotation after the fact, 
+        // so we dont have to deal with that messing up the previous calculations
+        let forward_dot = forward.dot(forward);
+        let parallel = (up.dot(forward) / forward_dot) * forward;
+        let orthogonal = up - parallel;
+        let w = forward.cross(orthogonal);
+        let orthogonal_magnitude = orthogonal.magnitude();
+
+        let x1 = cos_z / orthogonal_magnitude;
+        let x2 = sin_z / w.magnitude();
+        let orthogonal_rotated = orthogonal_magnitude * (x1 * orthogonal + x2 * w);
+        up = orthogonal_rotated + parallel;
+
+        up
     }
 }
 ```
 
-This code is not perfect. The camera slowly moves back when you rotate it. It works for our purposes, though. Feel free to improve it!
+This code is pretty simple - you can zoom in and out from the shape, and orbit around it. You might also want to edit your index buffer to add back facing vertices so you can see the shape from the back.
 
 We still need to plug this into our existing code to make it do anything. Add the controller to `State` and create it in `new()`.
 
@@ -384,7 +488,7 @@ struct State {
 impl<'a> State<'a> {
     async fn new(window: &'a Window) -> State<'a> {
         // ...
-        let camera_controller = CameraController::new(0.2);
+        let camera_controller = CameraController::new(0.05);
         // ...
 
         Self {
