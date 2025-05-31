2c917becc88ac566d30c3738f27525712861d723
Niels Saurer <me@nielssaurer.com>
2025-05-31 19:54:44 +0200

    replace buggy row-major with correct column-major opengl_to_wgpu matrix (#592)

--- code/beginner/tutorial8-depth/src/challenge.rs
+++ code/beginner/tutorial8-depth/src/challenge.rs
@@ -88,11 +88,11 @@ const DEPTH_VERTICES: &[Vertex] = &[
 const DEPTH_INDICES: &[u16] = &[0, 1, 2, 0, 2, 3];
 
 #[rustfmt::skip]
-pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.5,
-    0.0, 0.0, 0.0, 1.0,
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::from_cols(
+    cgmath::Vector4::new(1.0, 0.0, 0.0, 0.0),
+    cgmath::Vector4::new(0.0, 1.0, 0.0, 0.0),
+    cgmath::Vector4::new(0.0, 0.0, 0.5, 0.0),
+    cgmath::Vector4::new(0.0, 0.0, 0.5, 1.0),
 );
 
 const NUM_INSTANCES_PER_ROW: u32 = 10;
--- code/beginner/tutorial9-models/src/lib.rs
+++ code/beginner/tutorial9-models/src/lib.rs
@@ -19,11 +19,11 @@ mod texture;
 use model::{DrawModel, Vertex};
 
 #[rustfmt::skip]
-pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.5,
-    0.0, 0.0, 0.0, 1.0,
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::from_cols(
+    cgmath::Vector4::new(1.0, 0.0, 0.0, 0.0),
+    cgmath::Vector4::new(0.0, 1.0, 0.0, 0.0),
+    cgmath::Vector4::new(0.0, 0.0, 0.5, 0.0),
+    cgmath::Vector4::new(0.0, 0.0, 0.5, 1.0),
 );
 
 const NUM_INSTANCES_PER_ROW: u32 = 10;
