d7c18bf78f6ac973a8e0c5985020f3856efbacbb
Benjamin Hansen <bhbenjaminhansen@gmail.com>
2026-02-21 16:58:52 -0700

    debuging vuepress v2

--- FEATURES.md
+++ FEATURES.md
@@ -1,10 +1,12 @@
 # Features
 
 ## Planned
+
 * Framebuffer
 * code download links
 
 ## Things I'd like
+
 * Screen shots
 * Create gifs of examples running (?)
 * Displaying random models as wireframe and solid
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,93 +1,90 @@
-module.exports = {
-    base: '/learn-wgpu/',
-    title: 'Learn Wgpu',
-    theme: 'thindark',
-    public: 'res',
-    plugins: {
-        'vuepress-plugin-code-copy': true,
-        '@vuepress/back-to-top': true,
-        'seo': {
-            url: (_, $site, path) => ($site.themeConfig.domain || '') + path,
-        },
-    },
-    themeConfig: {
-        domain: '/learn-wgpu',
-        author: {
-            name: 'Benjamin Hansen',
-            twitter: 'https://twitter.com/sotrh760',
-        },
-        displayAllHeaders: false,
-        lastUpdated: 'Last Updated',
-        sidebar: [
-            '/',
-            {
-                title: 'Beginner',
-                collapsable: false,
-                children: [
-                    '/beginner/tutorial1-window/',
-                    '/beginner/tutorial2-surface/',
-                    '/beginner/tutorial3-pipeline/',
-                    '/beginner/tutorial4-buffer/',
-                    '/beginner/tutorial5-textures/',
-                    '/beginner/tutorial6-uniforms/',
-                    '/beginner/tutorial7-instancing/',
-                    '/beginner/tutorial8-depth/',
-                    '/beginner/tutorial9-models/',
-                ],
-            },
-            {
-                title: 'Intermediate',
-                collapsable: false,
-                children: [
-                    '/intermediate/tutorial10-lighting/',
-                    '/intermediate/tutorial11-normals/',
-                    '/intermediate/tutorial12-camera/',
-                    '/intermediate/tutorial13-hdr/',
-                ],
-            },
-            {
-                title: 'Compute Pipelines',
-                collapsable: true,
-                children: [
-                    '/compute/introduction/',
-                    '/compute/sorting/',
-                ]
-            },
-            {
-                title: 'Showcase',
-                collapsable: true,
-                children: [
-                    '/showcase/',
-                    '/showcase/mipmaps/',
-                    '/showcase/stencil/',
-                    '/showcase/windowless/',
-                    '/showcase/gifs/',
-                    '/showcase/pong/',
-                    '/showcase/alignment/',
-                    // '/showcase/compute/',
-                ]
-            },
-            {
-                title: 'News',
-                collapsable: true,
-                children: [
-                    '/news/28.0/',
-                    '/news/27.0/',
-                    '/news/26.0.1/',
-                    '/news/update-to-winit-0.30/',
-                    '/news/25.0/',
-                    '/news/24.0/',
-                    '/news/22.0/',
-                    '/news/0.18 and hdr/',
-                    '/news/0.17/',
-                    '/news/0.16/',
-                    '/news/0.15/',
-                    '/news/0.14/',
-                    '/news/0.13/',
-                    '/news/0.12/',
-                    '/news/pre-0.12/',
-                ]
-            }
-        ]
-    }
-}
+import { defineUserConfig } from "vuepress";
+import { viteBundler } from "@vuepress/bundler-vite";
+import { defaultTheme } from "@vuepress/theme-default";
+
+import WasmExample from "./components/WasmExample.vue"
+
+export default defineUserConfig({
+  base: "/learn-wgpu/",
+  title: "Learn Wgpu",
+  public: "res",
+  bundler: viteBundler({
+    vuePluginOptions: {}
+  }),
+  enhance: ({ app, router, siteData }) => {
+    app.component("WasmExample", WasmExample)
+  },
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "Beginner",
+        collapsable: false,
+        children: [
+          "/beginner/tutorial1-window/",
+          "/beginner/tutorial2-surface/",
+          "/beginner/tutorial3-pipeline/",
+          "/beginner/tutorial4-buffer/",
+          "/beginner/tutorial5-textures/",
+          "/beginner/tutorial6-uniforms/",
+          "/beginner/tutorial7-instancing/",
+          "/beginner/tutorial8-depth/",
+          "/beginner/tutorial9-models/",
+        ],
+      },
+      {
+        text: "Intermediate",
+        collapsable: false,
+        children: [
+          "/intermediate/tutorial10-lighting/",
+          "/intermediate/tutorial11-normals/",
+          "/intermediate/tutorial12-camera/",
+          "/intermediate/tutorial13-hdr/",
+        ],
+      },
+      {
+        text: "Compute Pipelines",
+        collapsable: true,
+        children: ["/compute/introduction/", "/compute/sorting/"],
+      },
+      {
+        text: "Showcase",
+        collapsable: true,
+        children: [
+          "/showcase/",
+          "/showcase/mipmaps/",
+          "/showcase/stencil/",
+          "/showcase/windowless/",
+          "/showcase/gifs/",
+          "/showcase/pong/",
+          "/showcase/alignment/",
+          // '/showcase/compute/',
+        ],
+      },
+      {
+        text: "News",
+        collapsable: true,
+        children: [
+          "/news/28.0/",
+          "/news/27.0/",
+          "/news/26.0.1/",
+          "/news/update-to-winit-0.30/",
+          "/news/25.0/",
+          "/news/24.0/",
+          "/news/22.0/",
+          "/news/0.18 and hdr/",
+          "/news/0.17/",
+          "/news/0.16/",
+          "/news/0.15/",
+          "/news/0.14/",
+          "/news/0.13/",
+          "/news/0.12/",
+          "/news/pre-0.12/",
+        ],
+      },
+    ],
+  }),
+});
