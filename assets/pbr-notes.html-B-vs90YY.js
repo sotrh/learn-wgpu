a8a0fe2962654c2f3ee41f89d1d4d51d8b1f5384
sotrh <sotrh@users.noreply.github.com>
2026-05-08 04:06:57 +0000

    Deploying to gh-pages from @ sotrh/learn-wgpu@008df3c90188de56696560d248c56efbafe2dccb 🚀

---
+++ assets/pbr-notes.html-B-vs90YY.js
@@ -0,0 +1,10 @@
+import { _ as n, c as o, b as e, o as s } from "./app-DiTjZiEr.js";
+const r = {};
+function a(c, t) {
+  return s(), o("div", null, [...t[0] || (t[0] = [e("h1", { id: "sources", tabindex: "-1" }, [e("a", { class: "header-anchor", href: "#sources" }, [e("span", null, "Sources")])], -1), e("ul", null, [e("li", null, "https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-shading/diffuse-lambertian-shading"), e("li", null, "http://jimmiejohnsson84.me/pages/rendering_pbr.html")], -1)])]);
+}
+const i = n(r, [["render", a]]), l = JSON.parse('{"path":"/intermediate/pbr-notes.html","title":"Sources","lang":"en-US","frontmatter":{},"git":{"updatedTime":1778213010000,"contributors":[{"name":"mud2monarch","username":"mud2monarch","email":"101775102+mud2monarch@users.noreply.github.com","commits":1,"url":"https://github.com/mud2monarch"}],"changelog":[{"hash":"008df3c90188de56696560d248c56efbafe2dccb","time":1778213010000,"email":"101775102+mud2monarch@users.noreply.github.com","author":"Zach Wong","message":"Correct some typos/out-of-sync in docs (#669)"}]},"filePathRelative":"intermediate/pbr-notes.md"}');
+export {
+  i as comp,
+  l as data
+};
