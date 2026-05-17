08c7d9b281a801177312078f81cf7e35d9c21cfc
sotrh <sotrh@users.noreply.github.com>
2026-05-17 03:29:09 +0000

    Deploying to gh-pages from @ sotrh/learn-wgpu@c88d6ab33b262d50499685ce9ebc6a1b5a8bce65 🚀

---
+++ assets/index.html-sUVOK65S.js
@@ -0,0 +1,10 @@
+import { _ as t, c as n, b as e, d as a, o as s } from "./app-HHbeU4D1.js";
+const r = {};
+function i(c, o) {
+  return s(), n("div", null, [...o[0] || (o[0] = [e("h1", { id: "foreword", tabindex: "-1" }, [e("a", { class: "header-anchor", href: "#foreword" }, [e("span", null, "Foreword")])], -1), e("p", null, [a("The articles in this section are not meant to be tutorials. They are showcases of the various things you can do with "), e("code", null, "wgpu"), a(". I won't go over the specifics of creating "), e("code", null, "wgpu"), a(" resources, as those will be covered elsewhere. The code for these examples is still available however and will be accessible on Github.")], -1)])]);
+}
+const h = t(r, [["render", i]]), d = JSON.parse('{"path":"/showcase/","title":"Foreword","lang":"en-US","frontmatter":{},"git":{"updatedTime":1778988332000,"contributors":[{"name":"Benjamin Hansen","username":"","email":"bhbenjaminhansen@gmail.com","commits":1}],"changelog":[{"hash":"c88d6ab33b262d50499685ce9ebc6a1b5a8bce65","time":1778988332000,"email":"bhbenjaminhansen@gmail.com","author":"Benjamin Hansen","message":"started text tutorial and exploring multithreading"}]},"filePathRelative":"showcase/README.md"}');
+export {
+  h as comp,
+  d as data
+};
---
+++ assets/pbr-notes.html-BhfdL6vz.js
@@ -0,0 +1,10 @@
+import { _ as a, c as t, b as e, o as s } from "./app-HHbeU4D1.js";
+const i = {};
+function r(o, n) {
+  return s(), t("div", null, [...n[0] || (n[0] = [e("h1", { id: "sources", tabindex: "-1" }, [e("a", { class: "header-anchor", href: "#sources" }, [e("span", null, "Sources")])], -1), e("ul", null, [e("li", null, "https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-shading/diffuse-lambertian-shading"), e("li", null, "http://jimmiejohnsson84.me/pages/rendering_pbr.html")], -1)])]);
+}
+const m = a(i, [["render", r]]), c = JSON.parse('{"path":"/intermediate/pbr-notes.html","title":"Sources","lang":"en-US","frontmatter":{},"git":{"updatedTime":1778988332000,"contributors":[{"name":"Benjamin Hansen","username":"","email":"bhbenjaminhansen@gmail.com","commits":1}],"changelog":[{"hash":"c88d6ab33b262d50499685ce9ebc6a1b5a8bce65","time":1778988332000,"email":"bhbenjaminhansen@gmail.com","author":"Benjamin Hansen","message":"started text tutorial and exploring multithreading"}]},"filePathRelative":"intermediate/pbr-notes.md"}');
+export {
+  m as comp,
+  c as data
+};
