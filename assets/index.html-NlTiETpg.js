c48258d06342fe82ec2f81c1f508b51af416f631
sotrh <sotrh@users.noreply.github.com>
2026-07-12 20:42:19 +0000

    Deploying to gh-pages from @ sotrh/learn-wgpu@ea9092c30599bd331af4ddba0300efeb4662b98b 🚀

---
+++ assets/index.html-NlTiETpg.js
@@ -0,0 +1,10 @@
+import { _ as n, c as t, b as e, o } from "./app-3iPpuz27.js";
+const s = {};
+function i(m, a) {
+  return o(), t("div", null, [...a[0] || (a[0] = [e("h1", { id: "update-to-0-17", tabindex: "-1" }, [e("a", { class: "header-anchor", href: "#update-to-0-17" }, [e("span", null, "Update to 0.17")])], -1), e("p", null, "No changes needed to the tutorial!", -1)])]);
+}
+const c = n(s, [["render", i]]), d = JSON.parse('{"path":"/news/0.17/","title":"Update to 0.17","lang":"en-US","frontmatter":{},"git":{"updatedTime":1783888716000,"contributors":[{"name":"Benjamin Hansen","username":"","email":"bhbenjaminhansen@gmail.com","commits":1}],"changelog":[{"hash":"ea9092c30599bd331af4ddba0300efeb4662b98b","time":1783888716000,"email":"bhbenjaminhansen@gmail.com","author":"Benjamin Hansen","message":"fixing code block"}]},"filePathRelative":"news/0.17/readme.md"}');
+export {
+  c as comp,
+  d as data
+};
