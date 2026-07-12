13a88a05b9d0a15e11347b9be5ad03ecbb8bb06b
sotrh <sotrh@users.noreply.github.com>
2026-07-12 20:33:26 +0000

    Deploying to gh-pages from @ sotrh/learn-wgpu@b30de9f0014ea4c370301c7a3a454737d0b73940 🚀

---
+++ assets/pbr-notes.html-m4O0X6gG.js
@@ -0,0 +1,10 @@
+import { _ as s, c as n, b as e, o as a } from "./app-EB07jUxz.js";
+const r = {};
+function o(i, t) {
+  return a(), n("div", null, [...t[0] || (t[0] = [e("h1", { id: "sources", tabindex: "-1" }, [e("a", { class: "header-anchor", href: "#sources" }, [e("span", null, "Sources")])], -1), e("ul", null, [e("li", null, "https://www.scratchapixel.com/lessons/3d-basic-rendering/introduction-to-shading/diffuse-lambertian-shading"), e("li", null, "http://jimmiejohnsson84.me/pages/rendering_pbr.html")], -1)])]);
+}
+const c = s(r, [["render", o]]), m = JSON.parse('{"path":"/intermediate/pbr-notes.html","title":"Sources","lang":"en-US","frontmatter":{},"git":{"updatedTime":1783888171000,"contributors":[{"name":"Ben Hansen","username":"","email":"sotrh@users.noreply.github.com","commits":1}],"changelog":[{"hash":"b30de9f0014ea4c370301c7a3a454737d0b73940","time":1783888171000,"email":"sotrh@users.noreply.github.com","author":"Ben Hansen","message":"30.0 (#676)"}]},"filePathRelative":"intermediate/pbr-notes.md"}');
+export {
+  c as comp,
+  m as data
+};
---
+++ assets/todo.html-DGn2cWW8.js
@@ -0,0 +1,10 @@
+import { _ as n, c as t, b as e, o as a } from "./app-EB07jUxz.js";
+const s = {};
+function r(c, o) {
+  return a(), t("div", null, [...o[0] || (o[0] = [e("h1", { id: "coming-soon", tabindex: "-1" }, [e("a", { class: "header-anchor", href: "#coming-soon" }, [e("span", null, "Coming Soon!")])], -1), e("p", null, "This section has not yet been completed.", -1)])]);
+}
+const i = n(s, [["render", r]]), l = JSON.parse('{"path":"/todo.html","title":"Coming Soon!","lang":"en-US","frontmatter":{},"git":{"updatedTime":1783888171000,"contributors":[{"name":"Ben Hansen","username":"","email":"sotrh@users.noreply.github.com","commits":1}],"changelog":[{"hash":"b30de9f0014ea4c370301c7a3a454737d0b73940","time":1783888171000,"email":"sotrh@users.noreply.github.com","author":"Ben Hansen","message":"30.0 (#676)"}]},"filePathRelative":"todo.md"}');
+export {
+  i as comp,
+  l as data
+};
