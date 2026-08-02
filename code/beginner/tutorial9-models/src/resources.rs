60b44e8de6653f3bbc7d950930f0a6a6f0b222a9
ZayadNimrod <32575659+ZayadNimrod@users.noreply.github.com>
2026-08-02 15:08:28 +0100

    Make normal loading more concise before we assume all models have normals

--- code/beginner/tutorial9-models/src/resources.rs
+++ code/beginner/tutorial9-models/src/resources.rs
@@ -112,38 +112,22 @@ pub async fn load_model(
         .into_iter()
         .map(|m| {
             let vertices = (0..m.mesh.positions.len() / 3)
-                .map(|i| {
-                    if m.mesh.normals.is_empty() {
-                        model::ModelVertex {
-                            position: [
-                                m.mesh.positions[i * 3],
-                                m.mesh.positions[i * 3 + 1],
-                                m.mesh.positions[i * 3 + 2],
-                            ],
-                            tex_coords: [
-                                m.mesh.texcoords[i * 2],
-                                1.0 - m.mesh.texcoords[i * 2 + 1],
-                            ],
-                            normal: [0.0, 0.0, 0.0],
-                        }
+                .map(|i| model::ModelVertex {
+                    position: [
+                        m.mesh.positions[i * 3],
+                        m.mesh.positions[i * 3 + 1],
+                        m.mesh.positions[i * 3 + 2],
+                    ],
+                    tex_coords: [m.mesh.texcoords[i * 2], 1.0 - m.mesh.texcoords[i * 2 + 1]],
+                    normal: if m.mesh.normals.is_empty() {
+                        [0.0, 0.0, 0.0]
                     } else {
-                        model::ModelVertex {
-                            position: [
-                                m.mesh.positions[i * 3],
-                                m.mesh.positions[i * 3 + 1],
-                                m.mesh.positions[i * 3 + 2],
-                            ],
-                            tex_coords: [
-                                m.mesh.texcoords[i * 2],
-                                1.0 - m.mesh.texcoords[i * 2 + 1],
-                            ],
-                            normal: [
-                                m.mesh.normals[i * 3],
-                                m.mesh.normals[i * 3 + 1],
-                                m.mesh.normals[i * 3 + 2],
-                            ],
-                        }
-                    }
+                        [
+                            m.mesh.normals[i * 3],
+                            m.mesh.normals[i * 3 + 1],
+                            m.mesh.normals[i * 3 + 2],
+                        ]
+                    },
                 })
                 .collect::<Vec<_>>();
 
